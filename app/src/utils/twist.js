9d6ba46cfa9cb47b6ac4c21cb0435c1eb8847baf
fanstrong <kingindoslayer@gmail.com>
2025-01-21 03:54:55 +0700

    Update twist.js

--- app/src/utils/twist.js
+++ app/src/utils/twist.js
@@ -22,7 +22,7 @@ class Twist {
     const _0x3f3194 = _0x1c034e.integral ?? '-';
     const _0x597bf8 = _0x27b4c1.evmWallet ?? undefined;
     this.twisters.put(_0x1dedc3, {
-      'text': "\n================== Account " + (_0x304df2 + 0x1) + " =================\nAddress      : - " + _0x1e5ed8 + " (MANGO) " + (_0x597bf8 ? "\n               - " + _0x27b4c1.evmAddress + " (EVM)" : '') + "\n\nBalance      : \nMANGO NETWORK : " + _0x24a5cd.map(_0x69cd16 => {
+      'text': "\n================== Account " + (_0x304df2 + 0x1) + " =================\nAddress         : - " + _0x1e5ed8 + " (MANGO) " + (_0x597bf8 ? "\n               - " + _0x27b4c1.evmAddress + " (EVM)" : '') + "\n\nBalance         : \nMANGO NETWORK   : " + _0x24a5cd.map(_0x69cd16 => {
         return "\n- " + _0x69cd16.totalBalance + " " + _0x69cd16.coinType.split('::').pop();
       }) + "\nSEPOLIA NETWORK : " + _0x5dfb8f.map(_0x9d80af => {
         return "\n- " + _0x9d80af.BALANCE + " " + _0x9d80af.SYMBOL;
