4b22d85cb6e5980df209d040c899bbf6d3da50d3
fanstrong <kingindoslayer@gmail.com>
2025-01-22 17:31:49 +0700

    Update twist.js

--- app/src/utils/twist.js
+++ app/src/utils/twist.js
@@ -22,13 +22,13 @@ class Twist {
     const _0x1504ab = _0x13137c.integral ?? '-';
     const _0x4d58d5 = _0x3f02a5.evmWallet ?? undefined;
     this.twisters.put(_0x8dad0a, {
-      'text': "\n================== Account " + (_0x45c52d + 0x1) + " =================\nAddress      : - " + _0x4caa34 + " (MANGO) " + (_0x4d58d5 ? "\n               - " + _0x3f02a5.evmAddress + " (EVM)" : '') + "\n\nBalance      : \nMANGO NETWORK : " + _0x25a428.map(_0x3b7a09 => {
+      'text': "\n================== Account " + (_0x45c52d + 0x1) + " =================\nAddress         : - " + _0x4caa34 + " (MANGO) " + (_0x4d58d5 ? "\n               - " + _0x3f02a5.evmAddress + " (EVM)" : '') + "\n\nBalance         : \nMANGO NETWORK   : " + _0x25a428.map(_0x3b7a09 => {
         return "\n- " + _0x3b7a09.totalBalance + " " + _0x3b7a09.coinType.split('::').pop();
       }) + "\nSEPOLIA NETWORK : " + _0x3b67d8.map(_0x53cecc => {
         return "\n- " + _0x53cecc.BALANCE + " " + _0x53cecc.SYMBOL;
       }) + "\nTBNB NETWORK    : " + _0x5159b6.map(_0x41b780 => {
         return "\n- " + _0x41b780.BALANCE + " " + _0x41b780.SYMBOL;
-      }) + "\n\nScore        : " + _0x1504ab + "\n               \nStatus : " + _0x27aad1 + "\nDelay : " + _0x1d8886 + "\n=============================================="
+      }) + "\n\nScore           : " + _0x1504ab + "\n               \nStatus : " + _0x27aad1 + "\nDelay  : " + _0x1d8886 + "\n=============================================="
     });
   }
   ["info"](_0x56ffc9 = '') {
